10d4f8250e9816de7df9f7045bd25b08f9197f6e
dlnguyen <spamespresso@outlook.com>
2017-01-19 10:38:19 +0800

    Create abbreviation.py

---
+++ abbreviation.py
@@ -0,0 +1,25 @@
+!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Supporting Python 2.7.16
+ 
+import sys, os, re
+ 
+try:    bibtexdb = open(sys.argv[1]).read()
+except: print("Error: specify the file to be processed!")
+ 
+if not os.path.isfile('journalList.txt'):
+    import urllib
+    urllib.urlretrieve("https://raw.githubusercontent.com/JabRef/jabref/master/src/main/resources/journals/journalList.txt", 
+            filename="journalList.txt")
+rulesfile = open('journalList.txt')
+ 
+for rule in rulesfile.readlines()[::-1]:           ## reversed alphabetical order matches extended journal names first
+    pattern1, pattern2 = rule.strip().split(" = ")
+    if pattern1 != pattern1.upper() and (' ' in pattern1):        ## avoid mere abbreviations
+        print("Replacing '%s' FOR '%s'" % (pattern1, pattern2))
+    #bibtexdb = bibtexdb.replace(pattern1.strip(), pattern2.strip())    ## problem - this is case sensitive
+        repl = re.compile(re.escape(pattern1), re.IGNORECASE)               ## this is more robust, although ca. 10x slower
+        bibtexdb = repl.sub(pattern2, bibtexdb)
+with open('abbreviated.bib', 'w') as outfile:
+    outfile.write(bibtexdb)
+    print "Bibtex database with abbreviated files saved into 'abbreviated.bib'"
